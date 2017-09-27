5484a95f993d8c9fc626a2da7a74f3298410269e
Philip N. Brown <philip.brown@gatech.edu>
2017-09-27 08:27:43 -0700

    wrote some cursory table definitions

---
+++ util/mysqlhelpers.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 20:38:21 2017
+
+@author: pnbrown
+"""
+
+
+import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector import DataError, DatabaseError, InterfaceError
+
+MYSQL_CONFIG
+
+TABLES = {}
+
+TABLES['accounts'] = (
+    "CREATE TABLE `accounts` ("
+    "  `acct_id` INT NOT NULL AUTO_INCREMENT,"
+    "  `name` VARCHAR(16) NOT NULL,"
+    "  `balance` INT NOT NULL DEFAULT 0,"
+    "  `in_genesis` BOOL NOT NULL DEFAULT FALSE,"
+    "  PRIMARY KEY (`acct_id`), UNIQUE KEY `name` (`name`),"
+    "  KEY `balance` (`balance`),"
+    "  KEY `in_genesis` (`in_genesis`))")
+
+TABLES['pending_genesis_confirms'] = (
+    "CREATE TABLE `pending_genesis_confirms` ("
+    "  `gc_id` INT NOT NULL AUTO_INCREMENT,"
+    "  `account` VARCHAR(16) NOT NULL,"
+    "  PRIMARY KEY (`gc_id`), UNIQUE KEY `account` (`account`))")
+
+TABLES['confirmer_accounts'] = (
+    "CREATE TABLE `confirmer_accounts` ("
+    "  `confirmer_id` INT NOT NULL AUTO_INCREMENT,"
+    "  `confirmer` VARCHAR(16) NOT NULL,"
+    "  `fees_collected` INT NOT NULL DEFAULT 0,"
+    "  PRIMARY KEY (`confirmer_id`), UNIQUE KEY `confirmer` (`confirmer`))")
+
+# idea: use "sending_account" for genesis confirms
+# and if 
+TABLES['confirmations'] = (
+    "CREATE TABLE `confirmations` ("
+    "  `conf_id` INT NOT NULL AUTO_INCREMENT,"
+    "  `ident` VARCHAR(1000) NOT NULL,"
+    "  `trxid` VARCHAR(40) NOT NULL,"
+    "  `type` ENUM('send', 'genesis_confirm'),"
+    "  `sending_account` VARCHAR(16) NOT NULL,"
+    "  `receiving_account` VARCHAR(16),"
+    "  `amount` INT NOT NULL,"
+    "  `new_sending_balance` INT DEFAULT NULL,"
+    "  `new_receiving_balance` INT DEFAULT NULL,"
+    "  `fee` INT NOT NULL DEFAULT 1,"
+    "  `confirmed_by` INT DEFAULT NULL,"
+    "  PRIMARY KEY (`conf_id`),"
+    "  KEY `ident` (`ident`),"
+    "  KEY `trxid` (`trxid`),"
+    "  FOREIGN KEY (confirmed_by) REFERENCES confirmer_accounts(confirmer_id))")
+
+TABLES['valid_sends'] = (
+    "CREATE TABLE `valid_sends` ("
+    "  `send_id` INT NOT NULL AUTO_INCREMENT,"
+    "  `trxid` VARCHAR(40) NOT NULL,"
+    "  `steem_block` INT NOT NULL,"
+    "  `time` DATETIME NOT NULL,"
+    "  `sending_account` VARCHAR(16) NOT NULL,"
+    "  `receiving_account` VARCHAR(16),"
+    "  `amount` INT NOT NULL,"
+    "  `fee` INT NOT NULL DEFAULT 1,"
+    "  `confirmation` INT DEFAULT NULL,"
+    "  PRIMARY KEY (`send_id`),"
+    "  KEY `sending_account` (`sending_account`),"
+    "  KEY `receiving_account` (`receiving_account`),"
+    "  KEY `time` (`time`),"
+    "  KEY `amount` (`amount`),"
+    "  FOREIGN KEY (confirmation) REFERENCES confirmations(conf_id))")
+
